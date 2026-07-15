4357de5871353f9e01aad7d19851241a260a372a
ro234bd <ro234bd@gmail.com>
2026-07-15 11:18:14 +0200

    Update sw.js v2

--- praxis-stuetz-haselgruber/sw.js
+++ praxis-stuetz-haselgruber/sw.js
@@ -1,48 +1,22 @@
-// sw.js - Version 3.61 FORCE UPDATE
-const CACHE_NAME = 'zeiterfassung-v3.61-secure'; // Neuer Name zwingt zum Neuladen
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+// sw.js - Völlig neutralisiert und alter Cache wird gelöscht
 
-// Installieren und altes Cache löschen
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Zwingt den Browser, den neuen Worker sofort zu nehmen
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  // Zwingt den Browser, diese neue (leere) Version sofort zu aktivieren
+  self.skipWaiting();
 });
 
-// Aktivieren und alte Caches aufräumen
 self.addEventListener('activate', (e) => {
+  // Löscht alle alten Offline-Speicher (Caches), die Probleme machen
   e.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('Alten Cache entfernen:', key);
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keyList.map((key) => caches.delete(key)));
     })
   );
-  return self.clients.claim();
+  self.clients.claim();
 });
 
-// Fetch-Strategie: Erst Netzwerk, dann Cache (damit Updates schneller sichtbar sind)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        // Neue Version vom Server holen und im Cache speichern
-        const resClone = res.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(() => caches.match(e.request)) // Falls offline, nimm Cache
-  );
+  // Der Türsteher macht Feierabend: 
+  // Alle Anfragen (Speichern, Laden) gehen direkt und unbearbeitet ans echte Internet
+  return;
 });
